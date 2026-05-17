2440f1fa464ffd40df05ac93e2811a514a03e7c7
Rahmad <rhmd76457645@gmail.com>
2026-05-17 21:56:37 +0700

    feat: menyelesaikan struktur migration database dan inisialisasi model

---
+++ database/migrations/2026_05_17_143014_create_pesanans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pesanans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('user')->restrictOnDelete();
+            $table->foreignId('meja_id')->constarined('mejas')->restrictOnDelete();
+            $table->string('nama_pelanggan',100);
+            $table->enum('status_pesanan',['Menunggu Dimasak','Sedang Dimasak','Siap dimasak','Siap Disajikan','Selesai'])->default('Menunggu Dimasak');
+            $table->decimal('total_harga', 12, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pesanans');
+    }
+};
